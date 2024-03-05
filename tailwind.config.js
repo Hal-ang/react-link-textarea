fc131152319b6d3dd289b87ba8f831e6fb28cf7e
Hal-ang <jpjgv05@gmail.com>
2024-03-05 15:01:28 +0900

    build: implement tailwind css output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
