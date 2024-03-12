f809e133ba406da07ab975d777dffcb9cfa9bfb9
Hal-ang <68503014+Hal-ang@users.noreply.github.com>
2024-03-12 19:02:56 +0900

    [Optimize-build] Apply rollup-plugin-terser for bundle zie optimization (#20)
    * build: set rollup-plugin-visualizer for bundle optimization
    
    * build: apply rollup-plugin-terser for bundle optimization
    
    * chore: Automated build commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 // rollup.config.js
-
 import autoprefixer from "autoprefixer";
 import babel from "@rollup/plugin-babel";
 import cssimport from "postcss-import";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default [
   {
@@ -33,8 +34,10 @@ export default [
       postcss({
         plugins: [cssimport(), autoprefixer()]
       }),
+      terser(),
       typescript(),
-      peerDepsExternal()
+      peerDepsExternal(),
+      visualizer()
     ],
     exclude: ["**/storybook/**", "**/stories/**", "**/stories/*.d.ts"]
   },
